2bd51f86774dc68db354537bba0ccc7e4b7e1d49
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-07 22:14:22 -0600

    refactor App component to use custom logo and add TodoItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from './platzi.webp';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <a
+          className="App-link"
+          href="https://platzi.com/reactjs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprendamos React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+function TodoItem() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Llorar con la Llorona</p>
+      <span>X</span>
+    </li>
+  );
+}
+
+export default App;
