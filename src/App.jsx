f532db6da4b1093a51d082f8c72c879451a4dad3
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-07 22:51:34 -0600

    refactor App component to include TodoCounter, TodoFilter, and CreateTodoButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import logo from './platzi.webp';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <TodoCounter/>
+      <TodoFilter/>
 
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoList>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      <CreateTodoButton/>
       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React
-        </a>
-      </header>
     </div>
   );
 }
