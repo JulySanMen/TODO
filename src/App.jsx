e5aa7865e7e3941cb0f30a91f0e5d5264a3a606f
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-08 22:31:40 -0600

    update TodoCounter component to accept completed and total props

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <TodoCounter/>
+      <TodoCounter completed={16} total={25}/>
       <TodoFilter/>
 
       <TodoList>
