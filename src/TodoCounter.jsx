aba318edb9f83dbb21a7e2fd2a3a652884925e68
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-10 21:29:47 -0600

    fix: comment out estilos object in TodoCounter component

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,14 +1,14 @@
-const estilos = {
+/*  const estilos = {
   backgroundColor: 'cyan',
   fontSize: '30px',
   textAlign: 'center',
   margin:0,
   padding:'48px'
-}
+}*/
 
 function TodoCounter({total, completed}) {
   return (
-    <h2 style={estilos}>
+    <h2 /*style={estilos}*/>
       Has completado {completed} de {total} TODOs</h2>
   );
 }
