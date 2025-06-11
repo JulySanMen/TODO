f7ef3166d313f99675bad59c67c10b4d7a7c5834
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-10 21:14:55 -0600

    fix: apply background color style to TodoCounter component

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,7 +1,11 @@
+const estilos = {
+  backgroundColor: 'blue',
+}
+
 function TodoCounter({total, completed}) {
   return (
-    
-    <h2>Has completado {completed} de {total} TODOs</h2>
+    <h2 style={{estilos}}>
+      Has completado {completed} de {total} TODOs</h2>
   );
 }
 
