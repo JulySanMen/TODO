c10842e4afb312c1ab77124b2a9a120e3d4a4ed4
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-15 22:20:51 -0600

    fix: update TodoList component styling for improved layout and responsiveness

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,11 @@
-import './TodoList.css'
+import './TodoList.css';
 
-function TodoList ({children}) {
-    return(
-        <ul className="TodoList">
-        {children}
-      </ul>
-    );
+function TodoList({ children }) {
+  return (
+    <ul className="TodoList">
+      {children}
+    </ul>
+  );
 }
-export {TodoList};
+
+export { TodoList };
