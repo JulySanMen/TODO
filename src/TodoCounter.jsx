3ffec72c2e4ec1f6f563a9f9466b6ef0c7b35966
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-15 21:54:50 -0600

    fix: enhance styling and layout for TodoCounter component

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -1,14 +1,6 @@
 import './TodoCounter.css';
 
-/*  const estilos = {
-  backgroundColor: 'cyan',
-  fontSize: '30px',
-  textAlign: 'center',
-  margin:0,
-  padding:'48px'
-}*/
-
-function TodoCounter({total, completed}) {
+function TodoCounter({ total, completed }) {
   return (
     <h1 className="TodoCounter">
       Has completado <span>{completed}</span> de <span>{total}</span> TODOs
