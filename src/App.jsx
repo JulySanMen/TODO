072e5b9ca1a7b3e3287ea07364e767e2ff27a307
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-15 22:22:35 -0600

    fix: enhance App component styling and layout for better user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
-import {TodoCounter} from './TodoCounter';
-import {TodoFilter} from './TodoFilter';
-import {TodoList} from './TodoList';
-import {TodoItem} from './TodoItem';
+import { TodoCounter } from './TodoCounter';
+import { TodoFilter } from './TodoFilter';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 import './App.css';
 
+// ✅ debe estar aquí
 const defaultTodos = [
-  {text: 'Cortar Cebolla', completed: true},
-  {text: 'Tomar el curso', completed: false},
-  {text: 'Llorar con la llorona', completed: false},
-  {text: 'Hacer comits', completed: false},
-  {text: 'Comer bien ', completed:false},
+  { text: 'Cortar Cebolla', completed: true },
+  { text: 'Tomar el curso', completed: false },
+  { text: 'Llorar con la llorona', completed: false },
+  { text: 'Hacer comits', completed: false },
+  { text: 'Comer bien ', completed: false },
 ];
 
 function App() {
   return (
-    <React.Fragment>
-      <TodoCounter completed={16} total={25}/>
-      <TodoFilter/>
-
+    <div className="TodoAppContainer">
+      <TodoCounter completed={16} total={25} />
+      <TodoFilter />
       <TodoList>
-        {defaultTodos.map(todo =>(<TodoItem 
-        key={todo.text} 
-        text={todo.text}
-        completed={todo.completed}
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
           />
-          ))}
+        ))}
       </TodoList>
-
-      <CreateTodoButton/> 
-      
-    </React.Fragment>
+      <CreateTodoButton />
+    </div>
   );
 }
-
 
 export default App;
