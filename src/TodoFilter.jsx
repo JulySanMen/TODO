e79b982451d09b257bfb28e90373b329b8206e1a
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-18 20:09:36 -0600

    fix: update logging in TodoFilter to include current search value

--- src/TodoFilter.jsx
+++ src/TodoFilter.jsx
@@ -4,8 +4,9 @@ import './TodoFilter.css';
 function TodoFilter() {
   //estado no solo se consume se actuliza 
   const [searchValue, setSearchValue] = React.useState('');
-  
-  console.log('Los usuarios buscan todos de  ')
+
+  console.log('Los usuarios buscan todos de '+ searchValue);
+
   return (
     <input
       placeholder="Buscar tareas..."
