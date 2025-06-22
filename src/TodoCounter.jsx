e83db1b7771512b1a6f42e3dc510cf1f6e5db96e
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-21 22:09:13 -0600

    fix: remove trailing whitespace in TodoCounter.jsx

--- src/TodoCounter.jsx
+++ src/TodoCounter.jsx
@@ -4,7 +4,7 @@ function TodoCounter({ total, completed }) {
   return (
     <h1 className="TodoCounter">
       Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-    </h1>
+    </h1> 
   );
 }
 
