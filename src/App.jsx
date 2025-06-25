e739583bf1e6175ca51d9fc0e79b6d93853728cf
JulySanMen <sanchezmendozajulieta@gmail.com>
2025-06-24 22:18:48 -0600

    fix: comment out defaultTodos and localStorage initialization for future reference

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 import './App.css'; 
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el Curso de Intro a React.js', completed: false },
-  { text: 'Llorar con la Llorona', completed: false },
-  { text: 'LALALALALA', completed: false },
-  { text: 'Usar estados derivados', completed: true },
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: true },
+// ];
 
-localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 // localStorage.removeItem('TODOS_V1');
 
 function App() {
